7759d248669a395173813576233ae8f723f5a81e
Filip Szlagowski <filip.szlagowski@gmail.com>
2025-12-16 09:22:39 +0100

    rename liczby to numbers

--- lib/algorithm.dart
+++ lib/algorithm.dart
@@ -1,10 +1,10 @@
-int? lookingForOutlierNumber(List<int> liczby) {
-  if (liczby.length < 3) {
+int? lookingForOutlierNumber(List<int> numbers) {
+  if (numbers.length < 3) {
     return null;
   }
   int countEven = 0;
   int countOdd = 0;
-  for (final n in liczby) {
+  for (final n in numbers) {
     if (n.isEven) {
       countEven++;
     } else {
@@ -12,14 +12,14 @@ int? lookingForOutlierNumber(List<int> liczby) {
     }
   }
   if (countEven == 1) {
-    for (final n in liczby) {
+    for (final n in numbers) {
       if (n.isEven) {
         return n;
       }
     }
   }
   if (countOdd == 1) {
-    for (final n in liczby) {
+    for (final n in numbers) {
       if (!n.isEven) {
         return n;
       }
